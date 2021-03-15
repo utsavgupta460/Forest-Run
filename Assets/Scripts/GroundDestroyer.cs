16db80643e3cab58cfc0ef4298d135456d43c295
UtsavGupta2412 <utsavgupta2412@gmail.com>
2021-03-16 02:16:56 +0530

    Full project

---
+++ Assets/Scripts/GroundDestroyer.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GroundDestroyer : MonoBehaviour
+{
+    private GameObject point;
+
+    void Start()
+    {
+        point = GameObject.Find("GroundEndPoint");
+    }
+
+    
+    void Update()
+    {
+        if(transform.position.x < point.transform.position.x)
+        {
+            gameObject.SetActive(false);
+        }
+    }
+}
